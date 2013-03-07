af034e32b70891d177c3b841b6c89ed0345d1741
Christopher Hesse <raymanfx@gmail.com>
2013-03-07 19:36:59 +0100

    FmRadioReceiver: diable until we fix bcm4330 FM

--- fuji.mk
+++ fuji.mk
@@ -79,8 +79,8 @@ PRODUCT_COPY_FILES += \
     $(NFCEE_ACCESS_PATH):system/etc/nfcee_access.xml
 
 # FM Radio
-PRODUCT_PACKAGES += \
-      FmRadioReceiver
+#PRODUCT_PACKAGES += \
+#      FmRadioReceiver
 
 PRODUCT_COPY_FILES += \
       frameworks/native/data/etc/com.stericsson.hardware.fm.receiver.xml:system/etc/permissions/com.stericsson.hardware.fm.receiver.xml
