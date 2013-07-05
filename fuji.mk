999828a492c6ed578ccb4ceb4fd3175a95687526
RaymanFX <raymanfx@gmail.com>
2013-07-05 23:31:49 +0200

    fuji: do not build libaudioutils

--- fuji.mk
+++ fuji.mk
@@ -94,8 +94,7 @@ PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio_policy.msm8660 \
     audio.primary.msm8660 \
-    audio_policy.conf \
-    libaudioutils
+    audio_policy.conf
 
 # GPS
 PRODUCT_PACKAGES += \
