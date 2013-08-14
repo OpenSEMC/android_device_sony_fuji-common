261e7d4798f017d0666100fbabbb2a0fbdae9e1a
RaymanFX <raymanfx@gmail.com>
2013-08-14 18:02:34 +0200

    fuji: more experimental display props

--- fuji.mk
+++ fuji.mk
@@ -245,15 +245,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # More display props - double check these!
 PRODUCT_PROPERTY_OVERRIDES += \
-    dev.pm.dyn_samplingrate=1
+    dev.pm.dyn_samplingrate=1 \
+    debug.hwc.dynThreshold=1.9
 
 # Low Power Audio Decoding
 PRODUCT_PROPERTY_OVERRIDES += \
     lpa.decode=true
-
-# Custom cache size
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hwui.text_cache_width=2048
 
 # Wifi
 PRODUCT_PROPERTY_OVERRIDES += \
