323a9a4c2c7896c1336bcdc40b2650b897dcc1e2
RaymanFX <raymanfx@gmail.com>
2013-09-16 19:55:20 +0200

    fuji: call BSP vendor blobs

--- fuji.mk
+++ fuji.mk
@@ -264,4 +264,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
     wifi.supplicant_scan_interval=15
 
 # Include non-opensource parts if available
-$(call inherit-product-if-exists, vendor/sony/fuji-common/fuji-common-vendor.mk)
+$(call inherit-product-if-exists, vendor/sony/fuji-common-bsp/fuji-common-vendor.mk)
