71062377741067ba3c5b0801dc55000df452307b
RaymanFX <raymanfx@gmail.com>
2013-10-27 19:41:03 +0100

    fuji/legacy: use different blobs

--- fuji.mk
+++ fuji.mk
@@ -253,4 +253,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
     wifi.supplicant_scan_interval=15
 
 # Include non-opensource parts if available
-$(call inherit-product-if-exists, vendor/sony/fuji-common-caf/fuji-common-vendor.mk)
+$(call inherit-product-if-exists, vendor/sony/fuji-common-mako/fuji-common-vendor.mk)
