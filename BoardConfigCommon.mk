3760c7c2ac7c0202438f4758806164cf3d722bea
Great-Geek <glendsouza2691995@hotmail.com>
2013-11-17 15:57:28 +0530

    Cortex A9 -> A8

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -44,10 +44,11 @@ BOARD_VENDOR := sony
 # Architecture
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT_CPU := cortex-a8
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
-TARGET_CPU_VARIANT := cortex-a9
+TARGET_CPU_VARIANT := cortex-a8
 TARGET_BOOTLOADER_BOARD_NAME := fuji
 TARGET_VENDOR_PLATFORM := fuji
 
