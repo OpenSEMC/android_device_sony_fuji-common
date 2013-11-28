026f473771b4b47143c9a310181d87abf29ed5f4
RaymanFX <raymanfx@gmail.com>
2013-11-28 16:34:27 +0100

    fuji-common: make us independent from sony_common

--- fuji.mk
+++ fuji.mk
@@ -169,11 +169,28 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/sony/fuji-common/rootdir/system/etc/thermald-semc.conf:system/etc/thermald-semc.conf
 
-# Xperia Settings (sony-common)
-#PRODUCT_PACKAGES += \
-#    XperiaSettings
+# Offline charging animation
+PRODUCT_COPY_FILES += \
+     device/sony/fuji-common/animations/charging_animation_01_X.png:system/semc/chargemon/data/charging_animation_01.png \
+     device/sony/fuji-common/animations/charging_animation_02_X.png:system/semc/chargemon/data/charging_animation_02.png \
+     device/sony/fuji-common/animations/charging_animation_03_X.png:system/semc/chargemon/data/charging_animation_03.png \
+     device/sony/fuji-common/animations/charging_animation_04_X.png:system/semc/chargemon/data/charging_animation_04.png \
+     device/sony/fuji-common/animations/charging_animation_05_X.png:system/semc/chargemon/data/charging_animation_05.png \
+     device/sony/fuji-common/animations/charging_animation_06_X.png:system/semc/chargemon/data/charging_animation_06.png \
+     device/sony/fuji-common/animations/charging_animation_07_X.png:system/semc/chargemon/data/charging_animation_07.png \
+     device/sony/fuji-common/animations/charging_animation_01_X.png:system/semc/chargemon/data/non-charging_animation_01.png \
+     device/sony/fuji-common/animations/charging_animation_02_X.png:system/semc/chargemon/data/non-charging_animation_02.png \
+     device/sony/fuji-common/animations/charging_animation_03_X.png:system/semc/chargemon/data/non-charging_animation_03.png \
+     device/sony/fuji-common/animations/charging_animation_04_X.png:system/semc/chargemon/data/non-charging_animation_04.png \
+     device/sony/fuji-common/animations/charging_animation_05_X.png:system/semc/chargemon/data/non-charging_animation_05.png \
+     device/sony/fuji-common/animations/charging_animation_06_X.png:system/semc/chargemon/data/non-charging_animation_06.png \
+     device/sony/fuji-common/animations/charging_animation_07_X.png:system/semc/chargemon/data/non-charging_animation_07.png
 
-# Extract recovery ramdisks (sony-common)
+# Boot Logo
+PRODUCT_COPY_FILES += \
+     device/sony/fuji-common/bootlogo/OpenSEMC2.rle:root/logo.rle
+
+# Extract recovery ramdisks
 PRODUCT_PACKAGES += \
     extract_elf_ramdisk
 
